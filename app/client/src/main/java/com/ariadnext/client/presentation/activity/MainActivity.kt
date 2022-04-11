f41fb089c85505314fa566d5755ccb07e2b2f91c
TMSafwen <strabelsi@exmind.fr>
2022-04-11 17:30:01 +0200

    [DI] Init hilt.

--- app/client/src/main/java/com/ariadnext/client/presentation/activity/MainActivity.kt
+++ app/client/src/main/java/com/ariadnext/client/presentation/activity/MainActivity.kt
@@ -3,10 +3,12 @@ package com.ariadnext.client.presentation.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ariadnext.client.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * The main activity.
  * */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
